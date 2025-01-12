2358abe08dcb7f5668392a9bcec309227b866f8d
Muhammad Bintang Mahardani <134298218+muhbintangmahardani@users.noreply.github.com>
2025-01-12 19:37:36 +0700

    Update estimasi_mobil.py

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -6,7 +6,7 @@ model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 st.title('Estimasi Harga Mobil Bekas')
 
 # Input data dari pengguna
-year = st.number_input('Input Tahun Mobil', min_value=0.0, max_value=2025, step=1)
+year = st.number_input('Input Tahun Mobil', min_value=1900, max_value=2025, step=1)
 mileage = st.number_input('Input Km Mobil', min_value=0.0, step=1.0)
 tax = st.number_input('Input Pajak Mobil', min_value=0.0, step=1.0)
 mpg = st.number_input('Input Konsumsi BBM Mobil', min_value=0.0, step=0.1)
