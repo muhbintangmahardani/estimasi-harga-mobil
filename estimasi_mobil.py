1b16239420c8c439a9964bf83d5b4d5d886fdb3f
Muhammad Bintang Mahardani <134298218+muhbintangmahardani@users.noreply.github.com>
2025-01-12 19:39:25 +0700

    Update estimasi_mobil.py

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -6,7 +6,7 @@ model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 st.title('Estimasi Harga Mobil Bekas')
 
 # Input data dari pengguna
-year = st.number_input('Input Tahun Mobil', min_value=1900, max_value=2025, step=1)
+year = st.number_input('Input Tahun Mobil', min_value=0, max_value=2025, step=1)
 mileage = st.number_input('Input Km Mobil', min_value=0.0, step=1.0)
 tax = st.number_input('Input Pajak Mobil', min_value=0.0, step=1.0)
 mpg = st.number_input('Input Konsumsi BBM Mobil', min_value=0.0, step=0.1)
@@ -15,9 +15,10 @@ engineSize = st.number_input('Input Engine Size', min_value=0.0, step=0.1)
 predict = ''
 
 # Validasi apakah semua input telah terisi
-if st.button('Estimasi Harga', disabled=(year == 1900 or mileage == 0.0 or tax == 0.0 or mpg == 0.0 or engineSize == 0.0)):
+if st.button('Estimasi Harga', disabled=(year == 0 or mileage == 0.0 or tax == 0.0 or mpg == 0.0 or engineSize == 0.0)):
     predict = model.predict(
         [[year, mileage, tax, mpg, engineSize]]
     )
     st.write('Estimasi harga mobil bekas dalam Ponds : ', predict)
     st.write('Estimasi harga mobil bekas dalam IDR (Juta) :', predict * 19000)
+
