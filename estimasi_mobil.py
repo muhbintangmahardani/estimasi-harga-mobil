d92dca3e1ee9dbe5f4913afb3460fa79f0236733
Muhammad Bintang Mahardani <134298218+muhbintangmahardani@users.noreply.github.com>
2025-01-12 21:59:11 +0700

    Update estimasi_mobil.py

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -19,17 +19,18 @@ st.text('Taufiq Ismail (17215032)')
 # Input data dari pengguna
 st.write("### Masukkan Data Mobil")
 year = st.number_input('Tahun Mobil', min_value=0, step=1, format="%d")
-mileage = st.number_input('Kilometer Mobil (Km)', min_value=0.0, step=1.0, format="%.2f")
-tax = st.number_input('Pajak Mobil (Pounds)', min_value=0.0, step=1.0, format="%.2f")
-mpg = st.number_input('Konsumsi BBM Mobil (MPG)', min_value=0.0, step=0.1, format="%.1f")
-engineSize = st.number_input('Ukuran Mesin (L)', min_value=0.0, step=0.1, format="%.1f")
+mileage = st.number_input('Kilometer Mobil (Km)', min_value=0, step=1, format="%d")
+tax = st.number_input('Pajak Mobil (Pounds)', min_value=0, step=1, format="%d")
+mpg = st.number_input('Konsumsi BBM Mobil (MPG)', min_value=0, step=1, format="%d")
+engineSize = st.number_input('Ukuran Mesin (L)', min_value=0, step=1, format="%d")
 
 # Prediksi Harga
 predict = ''
-if st.button('Estimasi Harga', disabled=(year == 0 or mileage == 0.0 or tax == 0.0 or mpg == 0.0 or engineSize == 0.0)):
+if st.button('Estimasi Harga', disabled=(year == 0 or mileage == 0 or tax == 0 or mpg == 0 or engineSize == 0)):
     predict = model.predict(
         [[year, mileage, tax, mpg, engineSize]]
     )
     st.write('### Hasil Estimasi')
-    st.write(f'Estimasi harga mobil bekas dalam Pounds: **£{predict[0]:,.2f}**')
-    st.write(f'Estimasi harga mobil bekas dalam IDR (Juta): **Rp{(predict[0] * 19000) / 1_000_000:,.2f} Juta**')
+    st.write(f'Estimasi harga mobil bekas dalam Pounds: **£{int(predict[0]):,}**')
+    st.write(f'Estimasi harga mobil bekas dalam IDR (Juta): **Rp{int(predict[0] * 19000) / 1_000_000:,} Juta**')
+
