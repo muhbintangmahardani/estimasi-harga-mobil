4c4738a0acbcac338945509c7ac1d1d743a9615d
Muhammad Bintang Mahardani <134298218+muhbintangmahardani@users.noreply.github.com>
2025-01-13 11:08:40 +0700

    Update estimasi_mobil.py

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -5,28 +5,43 @@ import streamlit as st
 model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 
 # Judul Aplikasi
-st.title('Universitas Bina Sarana Informatika')
-st.header('Project Machine Learning')
-st.subheader('Estimasi Harga Mobil Bekas')
-st.text('Menggunakan Algoritma:')
-st.text('- Linear Regression')
-st.text('- Decision Tree Regressor')
-st.text('- Deployment di Streamlit')
-st.text('Oleh:')
-st.text('Muh Bintang Mahardani (17225123)')
-st.text('Taufiq Ismail (17215032)')
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <h1>Universitas Bina Sarana Informatika</h1>
+        <h2>Project Machine Learning</h2>
+        <h3>Estimasi Harga Mobil Bekas</h3>
+        <p><b>Menggunakan Algoritma:</b></p>
+        <ul style="list-style: none; padding: 0;">
+            <li>- Linear Regression</li>
+            <li>- Decision Tree Regressor</li>
+            <li>- Deployment di Streamlit</li>
+        </ul>
+        <p><b>Oleh:</b></p>
+        <p>Muh Bintang Mahardani (17225123)</p>
+        <p>Taufiq Ismail (17215032)</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # Input data dari pengguna
 st.write("### Masukkan Data Mobil")
-year = st.number_input('Tahun Mobil', min_value=1900, step=1, format="%d")  # Changed min_value to 1900
-mileage = st.number_input('Kilometer Mobil (Km)', min_value=0, step=1, format="%d")
-tax = st.number_input('Pajak Mobil (Pounds)', min_value=0, step=1, format="%d")
-mpg = st.number_input('Konsumsi BBM Mobil (MPG)', min_value=0, step=1, format="%d")
-engineSize = st.number_input('Ukuran Mesin (L)', min_value=0, step=1, format="%d")
+year = st.number_input('Tahun Mobil', step=1, format="%d", value=None)
+mileage = st.number_input('Kilometer Mobil (Km)', min_value=0, step=1, format="%d", value=None)
+tax = st.number_input('Pajak Mobil (Pounds)', min_value=0, step=1, format="%d", value=None)
+mpg = st.number_input('Konsumsi BBM Mobil (MPG)', min_value=0, step=1, format="%d", value=None)
+engineSize = st.number_input('Ukuran Mesin (L)', min_value=0, step=1, format="%d", value=None)
+
+# Cek apakah semua input telah diisi
+inputs_filled = all(value is not None for value in [year, mileage, tax, mpg, engineSize])
+
+# Tampilkan peringatan jika tombol ditekan tanpa semua input terisi
+if st.button('Estimasi Harga') and not inputs_filled:
+    st.warning("Harap isi semua input terlebih dahulu!")
 
 # Prediksi Harga
-predict = ''
-if st.button('Estimasi Harga', disabled=(year == 0 or mileage == 0 or tax == 0 or mpg == 0 or engineSize == 0)):
+if inputs_filled:
     predict = model.predict(
         [[year, mileage, tax, mpg, engineSize]]
     )
